0d09bd2559f8c02c3ed0fd23734a407562c3e4e5
BMenez <bmenez@laposte.net>
2025-07-31 21:08:44 +0200

    Data

--- src/app/cryptos.js
+++ src/app/cryptos.js
@@ -12,13 +12,13 @@ export const cryptos = [
   { crypto: "fetch-ai", symbol: "FET", montant: 1539.37313712, investi: 1500 },
   { crypto: "bittensor", symbol: "TAO", montant: 10.21288743, investi: 3000 },
   { crypto: "reserve-rights-token", symbol: "RSR", montant: 393780.6252, investi: 3000 },    // RSR
-  { crypto: "syrup", symbol: "SYRUP", montant: 2876.16, investi: 1000 },           // SYRUP (vérifier nom exact)
+  { crypto: "syrup", symbol: "SYRUP", montant: 2376.16, investi: 1000 },           // SYRUP (vérifier nom exact)
   { crypto: "akash-network", symbol: "AKT", montant: 814.74656, investi: 1500 },     // AKT
   { crypto: "nervos-network", symbol: "CKB", montant: 68140.84731328, investi: 500 },    // CKB
   { crypto: "gamercoin", symbol: "GHX", montant: 5460.63, investi: 500 },          // GHX (à vérifier)
   { crypto: "aerodrome-finance", symbol: "AERO", montant: 1247.21443, investi: 1000},
   { crypto: "kaspa", symbol: "KAS", montant: 11150.838, investi: 1000 },
-  { crypto: "io", symbol: "IO", montant: 1148.40872937, investi: 800 },
+  { crypto: "io", symbol: "IO", montant: 1479.14765937, investi: 1000 },
   { crypto: "alephium", symbol: "ALPH", montant: 1646.31145, investi: 500 },
   { crypto: "coredaoorg", symbol: "CORE", montant: 1118.21067, investi: 500 },
   { crypto: "ankr", symbol: "ANKR", montant: 29623.6593873, investi: 500 },
