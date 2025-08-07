fb48dd1c27421285e53422d954d41c4207f32553
BMenez <bmenez@laposte.net>
2025-08-07 10:18:53 +0200

    data

--- src/app/cryptos.js
+++ src/app/cryptos.js
@@ -11,7 +11,7 @@ export const cryptos = [
   { crypto: "stacks", symbol: "STX", montant: 746.99357, investi: 500 },
   { crypto: "fetch-ai", symbol: "FET", montant: 1539.37313712, investi: 1500 },
   { crypto: "bittensor", symbol: "TAO", montant: 10.21288743, investi: 3000 },
-  { crypto: "reserve-rights-token", symbol: "RSR", montant: 393780.6252, investi: 3000 },    // RSR
+  { crypto: "reserve-rights-token", symbol: "RSR", montant: 392530.6252, investi: 3000 },    // RSR
   { crypto: "syrup", symbol: "SYRUP", montant: 2376.16, investi: 1000 },           // SYRUP (vérifier nom exact)
   { crypto: "akash-network", symbol: "AKT", montant: 814.74656, investi: 1500 },     // AKT
   { crypto: "nervos-network", symbol: "CKB", montant: 68140.84731328, investi: 500 },    // CKB
