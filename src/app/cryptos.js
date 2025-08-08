c51596a44dac1b0ca42c7f61a2356c5136d33e48
BMenez <bmenez@laposte.net>
2025-08-08 09:06:08 +0200

    data

--- src/app/cryptos.js
+++ src/app/cryptos.js
@@ -12,10 +12,10 @@ export const cryptos = [
   { crypto: "fetch-ai", symbol: "FET", montant: 1539.37313712, investi: 1500 },
   { crypto: "bittensor", symbol: "TAO", montant: 10.21288743, investi: 3000 },
   { crypto: "reserve-rights-token", symbol: "RSR", montant: 392530.6252, investi: 3000 },    // RSR
-  { crypto: "syrup", symbol: "SYRUP", montant: 2376.16, investi: 1000 },           // SYRUP (vérifier nom exact)
+  { crypto: "syrup", symbol: "SYRUP", montant: 2356.16, investi: 1000 },           // SYRUP (vérifier nom exact)
   { crypto: "akash-network", symbol: "AKT", montant: 814.74656, investi: 1500 },     // AKT
-  { crypto: "nervos-network", symbol: "CKB", montant: 68140.84731328, investi: 500 },    // CKB
-  { crypto: "gamercoin", symbol: "GHX", montant: 5460.63, investi: 500 },          // GHX (à vérifier)
+  { crypto: "nervos-network", symbol: "CKB", montant: 68035.84731328, investi: 500 },    // CKB
+  { crypto: "gamercoin", symbol: "GHX", montant: 5282.63, investi: 500 },          // GHX (à vérifier)
   { crypto: "aerodrome-finance", symbol: "AERO", montant: 1247.21443, investi: 1000},
   { crypto: "kaspa", symbol: "KAS", montant: 11150.838, investi: 1000 },
   { crypto: "io", symbol: "IO", montant: 1479.14765937, investi: 1000 },
