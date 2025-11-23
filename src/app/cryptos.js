be12a18c288533e2a594b398617b866fcb8c8a2c
BMenez <bmenez@laposte.net>
2025-11-23 11:13:06 +0100

    Data - reset pf

--- src/app/cryptos.js
+++ src/app/cryptos.js
@@ -1,35 +1,20 @@
 export const cryptosSet1 = [
-  { crypto: "bitcoin", symbol: "BTC", montant: 0.01095294, investi: 1000 },
-  { crypto: "ethereum", symbol: "ETH", montant: 0.47071980, investi: 2000 },
-  { crypto: "usd-coin", symbol: "USDC", montant: 4015.604258, investi: 0 },
+  { crypto: "bitcoin", symbol: "BTC", montant: 0.01648839, investi: 1500 },
+  { crypto: "ethereum", symbol: "ETH", montant: 0.62456994, investi: 2500 },
+  { crypto: "usd-coin", symbol: "USDC", montant: 7048.153655, investi: 0 },
   { crypto: "chainlink", symbol: "LINK", montant: 89.10561383, investi: 1500 },
   { crypto: "aave", symbol: "AAVE", montant: 6.25493528, investi: 1000 },
-  { crypto: "yearn-finance", symbol: "YFI", montant: 0.16498436, investi: 1000 },
-  { crypto: "injective-protocol", symbol: "INJ", montant: 135.82718541, investi: 2000 },
   { crypto: "pendle", symbol: "PENDLE", montant: 222.90514024, investi: 1000 },
-  { crypto: "render-token", symbol: "RNDR", montant: 226.08495259, investi: 1000 },
   { crypto: "blockstack", symbol: "STX", montant: 1351.485032, investi: 1000 },
-  { crypto: "bittensor", symbol: "TAO", montant: 10.21288743, investi: 3000 },
-  { crypto: "reserve-rights-token", symbol: "RSR", montant: 392530.6252, investi: 3000 },    // RSR
+  { crypto: "bittensor", symbol: "TAO", montant: 10.92417543, investi: 3000 },
   { crypto: "syrup", symbol: "SYRUP", montant: 2356.16, investi: 1000 },           // SYRUP (vérifier nom exact)
   { crypto: "akash-network", symbol: "AKT", montant: 814.22656, investi: 1500 },     // AKT
   { crypto: "nervos-network", symbol: "CKB", montant: 182658.32731328, investi: 1000 },    // CKB
-  { crypto: "gamercoin", symbol: "GHX", montant: 5282.63, investi: 500 },          // GHX (à vérifier)
   { crypto: "aerodrome-finance", symbol: "AERO", montant: 1247.21443, investi: 1000},
   { crypto: "kaspa", symbol: "KAS", montant: 14958.027, investi: 1000 },
-  { crypto: "io", symbol: "IO", montant: 1479.14765937, investi: 1000 },
-  { crypto: "alephium", symbol: "ALPH", montant: 3583.61221, investi: 1000 },
-  { crypto: "coredaoorg", symbol: "CORE", montant: 1866.56067, investi: 1000 },
-  { crypto: "ankr", symbol: "ANKR", montant: 55073.1678039, investi: 1000 },
-  { crypto: "qubic-network", symbol: "QUBIC", montant: 482537356.46745, investi: 1000 },
-  { crypto: "nosana", symbol: "NOS", montant: 1381.28, investi: 1000 },
-  { crypto: "trakxMemes", symbol: "TRAKX-MEMES", montant: 4.03, investi: 1000 },
-  { crypto: "trakxGaming", symbol: "TRAKX-GAMING", montant: 13.27, investi: 500 },
   { crypto: "defit", symbol: "DEFIT", montant: 4500, investi: 0 },
-  { crypto: "jito-governance-token", symbol: "JTO", montant: 520.46, investi: 1000 },
   { crypto: "ondo-finance", symbol: "ONDO", montant: 968.97792815, investi: 1000 },
   { crypto: "solana", symbol: "SOL", montant: 5.04463884, investi: 1000 },
-  { crypto: "pyth-network", symbol: "PYTH", montant: 2808.5886, investi: 500 },
 ];
 
 export const cryptosSet2 = [
