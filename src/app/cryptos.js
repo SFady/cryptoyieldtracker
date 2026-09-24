786391f807adf55a642328d06887b4287ccd2303
Sylvain FADY <sylvain.fady@cgi.com>
2026-09-24 15:39:14 +0200

    Values

--- src/app/cryptos.js
+++ src/app/cryptos.js
@@ -25,8 +25,8 @@ export const cryptosSet2 = [
 
 export const cryptosSet4 = [
   { crypto: "aave", symbol: "AAVE", montant: 1.2, investi: 100 },
-  { crypto: "bitcoin", symbol: "BTC", montant: 0.0015, investi: 100 },
-  { crypto: "ethereum", symbol: "ETH", montant: 0.033, investi: 100 },
+  { crypto: "bitcoin", symbol: "BTC", montant: 0.00225, investi: 100 },
+  { crypto: "ethereum", symbol: "ETH", montant: 0.0495, investi: 100 },
   { crypto: "chainlink", symbol: "LINK", montant: 9.34, investi: 100 },
   { crypto: "ondo-finance", symbol: "ONDO", montant: 89.26, investi: 50 },
   { crypto: "pendle", symbol: "PENDLE", montant: 17, investi: 50 },
