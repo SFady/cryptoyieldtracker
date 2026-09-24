29aba20082f91571f82fc681b67c975cfc211146
Sylvain FADY <sylvain.fady@cgi.com>
2026-09-24 14:28:23 +0200

    Infamous

--- src/app/cryptos.js
+++ src/app/cryptos.js
@@ -27,7 +27,12 @@ export const cryptosSet4 = [
   { crypto: "bitcoin", symbol: "BTC", montant: 0.00187141, investi: 116 },
   { crypto: "ethereum", symbol: "ETH", montant: 0.02414785, investi: 116 },
   { crypto: "bittensor", symbol: "TAO", montant: 0.3, investi: 116 },
-  { crypto: "stax-token", symbol: "STAX", montant: 135, investi: 100 },
+  { crypto: "stax-token", symbol: "STAX", montant: 1, investi: 1 },
+  { crypto: "chainlink", symbol: "LINK", montant: 1, investi: 1 },
+  { crypto: "aave", symbol: "AAVE", montant: 1, investi: 1 },
+  { crypto: "solana", symbol: "SOL", montant: 1, investi: 1 },
+  { crypto: "pendle", symbol: "PENDLE", montant: 1, investi: 1 },
+  { crypto: "ondo-finance", symbol: "ONDO", montant: 1, investi: 1 },
 
 ];
 
