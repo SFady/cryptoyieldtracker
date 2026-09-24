dc7a1d389f30800a7657e7a96038d7cfb8536624
Sylvain FADY <sylvain.fady@cgi.com>
2026-09-24 15:06:46 +0200

    Values Infamous

--- src/app/cryptos.js
+++ src/app/cryptos.js
@@ -27,11 +27,11 @@ export const cryptosSet4 = [
   { crypto: "aave", symbol: "AAVE", montant: 1.2, investi: 100 },
   { crypto: "bitcoin", symbol: "BTC", montant: 0.0015, investi: 100 },
   { crypto: "ethereum", symbol: "ETH", montant: 0.033, investi: 100 },
-  { crypto: "chainlink", symbol: "LINK", montant: 1, investi: 1 },
-  { crypto: "ondo-finance", symbol: "ONDO", montant: 1, investi: 1 },
-  { crypto: "pendle", symbol: "PENDLE", montant: 1, investi: 1 },
-  { crypto: "solana", symbol: "SOL", montant: 1, investi: 1 },
-  { crypto: "blockstack", symbol: "STX", montant: 1, investi: 1 },
+  { crypto: "chainlink", symbol: "LINK", montant: 9.34, investi: 100 },
+  { crypto: "ondo-finance", symbol: "ONDO", montant: 89.26, investi: 50 },
+  { crypto: "pendle", symbol: "PENDLE", montant: 17, investi: 50 },
+  { crypto: "solana", symbol: "SOL", montant: 0.413, investi: 50 },
+  { crypto: "blockstack", symbol: "STX", montant: 138.7, investi: 50 },
   { crypto: "bittensor", symbol: "TAO", montant: 0.27, investi: 100 },
 ];
 
